71592e4291171ba6659f87b7534ead78db94d8eb
Kavya021005 <kavya94153@gmail.com>
2026-02-07 12:10:43 +0530

    Modern upload UI integrated and CSV upload fixed

--- backend/equipment/urls.py
+++ backend/equipment/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from .views import DatasetListView, DatasetRecordsView, UploadCSVView
 from .views import DatasetPDFView
-
+from django.urls import path
+from .views import (
+    DatasetListView,
+    DatasetRecordsView,
+    UploadCSVView,
+    DatasetPDFView
+)
 
 urlpatterns = [
     path("datasets/", DatasetListView.as_view()),
     path("datasets/<int:dataset_id>/records/", DatasetRecordsView.as_view()),
-    path("upload/", UploadCSVView.as_view()),
+    path("upload-csv/", UploadCSVView.as_view(), name="upload-csv"),
     path("datasets/<int:dataset_id>/pdf/", DatasetPDFView.as_view()),
 
 ]
